1c1e9c22d949e446660bc54f02c9cd721295d059
Eric Neves <ericnevesrodrigues@gmail.com>
2024-05-01 21:27:48 -0400

    Refactor: change entity - user

--- app/src/entities/User.ts
+++ app/src/entities/User.ts
@@ -1,11 +1,6 @@
-class User {
-  public id!: string;
-
-  public name!: string;
-  public email!: string;
-  public password!: string;
-
-  constructor(props: User) {
-    Object.assign(this, props)
-  }
+export interface User {
+  id?: string; 
+  name: string;
+  email: string;
+  password: string;
 }
