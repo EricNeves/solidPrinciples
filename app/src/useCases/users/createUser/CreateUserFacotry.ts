d07972925dcc9776511d637aa7bfd9be3707a501
Eric Neves <ericnevesrodrigues@gmail.com>
2024-05-03 23:51:56 -0400

    Refactor: inject bcrypt class

--- app/src/useCases/users/createUser/CreateUserFacotry.ts
+++ app/src/useCases/users/createUser/CreateUserFacotry.ts
@@ -1,17 +1,18 @@
 import { PostgresUserRepository } from "../../../repositories/features/PostgresUserRepository";
-import { Bcrypt } from "../../../utils/bcrypt/features/Bcrypt";
 import { Validator } from "../../../utils/validation/features/Validator";
+import { BCryptHelper } from '../../../utils/bcrypt/features/index'
+import { Bcrypt } from "../../../utils/bcrypt/features/Bcrypt";
 import { CreateUserController } from "./CreateUserController";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt'
 import Joi from "joi";
+
 
 export class CreateUserFactory {
   static generateInstance() {
     const postgresUserRepository = new PostgresUserRepository(
       new PrismaClient(),
-      new Bcrypt(bcrypt)
+      new Bcrypt(new BCryptHelper())
     );
 
     const userSchema = Joi.object({
@@ -20,7 +21,7 @@ export class CreateUserFactory {
       password: Joi.string().min(4).required(),
     });
 
-    const bcryptDep            = new Bcrypt(bcrypt)
+    const bcryptDep            = new Bcrypt(new BCryptHelper())
     const createUserUseCase    = new CreateUserUseCase(postgresUserRepository, bcryptDep);
     const validator            = new Validator(userSchema);
     const createUserController = new CreateUserController(
