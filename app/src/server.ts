8eaf54d46ed697205074dd5788c71edeb106905e
Eric Neves <ericnevesrodrigues@gmail.com>
2024-05-06 12:08:27 -0400

    Feat: add cors

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,25 +1,31 @@
 import express, { Express, NextFunction, Request, Response } from "express";
-import { serve, setup } from "swagger-ui-express"
+import { serve, setup } from "swagger-ui-express";
 import "express-async-errors";
+import cors from "cors";
 
-import { handleError } from "./middlewares/handleError"
-import swaggerSchema from './swagger.json'
- 
+import { handleError } from "./middlewares/handleError";
+import swaggerSchema from "./swagger.json";
+
 const app: Express = express();
 
 import userRouter from "./routes/user";
 
 const PORT = process.env.PORT || 3030;
 
+app.use(
+  cors({
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
-app.use("/doc", serve, setup(swaggerSchema))
+app.use("/doc", serve, setup(swaggerSchema));
 app.use("/users", userRouter);
 app.use(handleError);
 app.use((request: Request, response: Response, next: NextFunction) => {
   response.status(404).json({
-    message: "Sorry, endpoint not found."
-  })
-})
+    message: "Sorry, endpoint not found.",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on PORT ${PORT}`);
