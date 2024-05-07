90906458d91725ec28aa1c91fcc0d459066064ed
Eric Neves <ericnevesrodrigues@gmail.com>
2024-05-06 20:37:12 -0400

    Fix: resolve command

--- app/start.sh
+++ app/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+pnpm install
+
+pnpm run build
+
 if [ "$env" = "development" ]; then \
     pnpm run migrate:dev; \
 else \
     pnpm run migrate:prod; \
 fi
-
-pnpm install
-
-pnpm run build
 
 pnpm start
