17328e2c1f904148e88641f318f1deca750cd116
Eric Neves <ericnevesrodrigues@gmail.com>
2024-05-10 14:40:04 -0400

    Feat: update start.sh

--- app/start.sh
+++ app/start.sh
@@ -4,10 +4,6 @@ pnpm install
 
 pnpm run build
 
-if [ "$env" = "development" ]; then \
-    pnpm run migrate:dev; \
-    pnpm run dev; \
-else \
-    pnpm run migrate:prod; \
-    pnpm start; \
-fi
+pnpm run migrate:prod
+
+pnpm start
